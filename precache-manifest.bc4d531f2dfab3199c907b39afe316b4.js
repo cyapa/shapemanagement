33fd9e2b99ee1d7a659f6521bb8380534e4d4862
Chathura <kymcwyapa@gmail.com>
2020-03-25 12:45:25 +0200

    Updates

---
+++ precache-manifest.bc4d531f2dfab3199c907b39afe316b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ad302f9a4479b8291b28bdf7a075c5",
+    "url": "/shapemanagement/index.html"
+  },
+  {
+    "revision": "79dab2d3908eba75e392",
+    "url": "/shapemanagement/static/css/main.015dae99.chunk.css"
+  },
+  {
+    "revision": "4bf3fcfba8b084141a38",
+    "url": "/shapemanagement/static/js/2.ae069e08.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/shapemanagement/static/js/2.ae069e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79dab2d3908eba75e392",
+    "url": "/shapemanagement/static/js/main.67eb3fd2.chunk.js"
+  },
+  {
+    "revision": "40cbda0a6507c9716195",
+    "url": "/shapemanagement/static/js/runtime-main.d2a34d5e.js"
+  }
+]);
